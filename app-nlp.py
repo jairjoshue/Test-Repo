edf0717c1f361cc156b9d8da4dd35254d6fe184d
jairjoshue <41520295+jairjoshue@users.noreply.github.com>
2025-03-01 14:51:39 -0500

    Update app-nlp.py

--- app-nlp.py
+++ app-nlp.py
@@ -130,19 +130,14 @@ st.markdown("""
     <p style="font-size: 8px; font-style: italic;">
     Nombre: Jairsinho Patiño
     Documento: 10010010
-
     Nombre: Juan Perez
     Documento: 20020020
-
     Nombre: Pepe Guzman
     Documento: 30030030
-
     Nombre: Manuel Burga
     Documento: 40040040
-
     Nombre: Maria Cuadro
     Documento: 50050050
-
     Nombre: Jose Machicao
     Documento: 60060060
     </p>
